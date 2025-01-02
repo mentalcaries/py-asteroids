65e38486293b461955fbb6557d215f5d0fd7ffb2
mentalcaries <neexis@gmail.com>
2025-01-02 17:53:36 -0400

    rate limit shots

---
+++ circleshape.py
@@ -0,0 +1,24 @@
+import pygame
+
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass 
+
+    def is_colliding_with(self, other):
+        distance_between = self.position.distance_to(other.position)
+        return distance_between <= self.radius + other.radius
--- main.py
+++ main.py
@@ -1,16 +1,49 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for object in updatable:
+            object.update(dt)
+
+        for object in asteroids:
+            if object.is_colliding_with(player):
+                print("Game over!")
+                sys.exit()
+                
         screen.fill("black")
+        
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 
 
